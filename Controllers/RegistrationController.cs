ed8fff6ed7103291cbf214a4b3704cee36e3f6b2
Jessica McKinney <61521950+jmckinney48045@users.noreply.github.com>
2020-07-15 18:27:00 -0400

    Add files via upload

---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LAB21.Models
+{
+    public class RegistrationController : Controller
+    {
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Registration registration)
+        {
+            //save to database, or present back to user
+
+            return View(registration);
+        }
+    }
+}
